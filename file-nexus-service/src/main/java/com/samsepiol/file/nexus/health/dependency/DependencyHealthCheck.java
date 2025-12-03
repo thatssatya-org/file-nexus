fd30fb3a6b49778501bf50a7b635e05ce79a93de
Satyajit <thatssatya@gmail.com>
2025-12-04 04:54:24 +0530

    Temporal Dependency health check fixed

--- file-nexus-service/src/main/java/com/samsepiol/file/nexus/health/dependency/DependencyHealthCheck.java
+++ file-nexus-service/src/main/java/com/samsepiol/file/nexus/health/dependency/DependencyHealthCheck.java
@@ -1,5 +1,6 @@
 package com.samsepiol.file.nexus.health.dependency;
 
+// TODO move to library, introduce dependency tiers for reporting failure
 public interface DependencyHealthCheck {
     boolean isHealthy();
 }
--- file-nexus-service/src/main/java/com/samsepiol/file/nexus/health/dependency/impl/TemporalServiceHealthCheck.java
+++ file-nexus-service/src/main/java/com/samsepiol/file/nexus/health/dependency/impl/TemporalServiceHealthCheck.java
@@ -1,23 +1,28 @@
 package com.samsepiol.file.nexus.health.dependency.impl;
 
 import com.samsepiol.file.nexus.health.dependency.DependencyHealthCheck;
+import io.temporal.api.workflowservice.v1.GetSystemInfoRequest;
+import io.temporal.serviceclient.WorkflowServiceStubs;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Service;
 
 @Service
+@ConditionalOnBean(WorkflowServiceStubs.class)
 @RequiredArgsConstructor
 @Slf4j
 public class TemporalServiceHealthCheck implements DependencyHealthCheck {
-    // TODO
-//    private final TemporalHealthCheck temporal;
+    private final WorkflowServiceStubs service;
 
     @Override
     public boolean isHealthy() {
-//        boolean healthy = temporal.isHealthy();
-//        if (!healthy) {
-//            log.error("[Health Check] Temporal unhealthy");
-//        }
-        return true;
+        try {
+            var systemInfo = service.blockingStub().getSystemInfo(GetSystemInfoRequest.getDefaultInstance());
+            return systemInfo.isInitialized();
+        } catch (Exception e) {
+            log.error("[Health Check] Temporal unhealthy");
+            return Boolean.FALSE;
+        }
     }
 }
