01ed40f6c5ccb13bc2ffd4a1c59c5de263f5c266
Satyajit <thatssatya@gmail.com>
2025-12-05 20:41:02 +0530

    Local docker compose changes

--- file-nexus-controller/src/main/java/com/samsepiol/file/nexus/health/service/impl/DefaultHealthCheckService.java
+++ file-nexus-controller/src/main/java/com/samsepiol/file/nexus/health/service/impl/DefaultHealthCheckService.java
@@ -2,18 +2,30 @@ package com.samsepiol.file.nexus.health.service.impl;
 
 import com.samsepiol.file.nexus.health.service.HealthCheckService;
 import com.samsepiol.file.nexus.health.service.dependency.DependencyHealthCheck;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DefaultHealthCheckService implements HealthCheckService {
     private final List<DependencyHealthCheck> dependencyHealthChecks;
 
+    @PostConstruct
+    public void init() {
+        isHealthy();
+    }
+
     @Override
     public boolean isHealthy() {
-        return dependencyHealthChecks.stream().allMatch(DependencyHealthCheck::isHealthy);
+        return dependencyHealthChecks.stream()
+                .allMatch(dependencyHealthCheck -> {
+                    log.info("[Health Check] - {} - {}", dependencyHealthCheck.getClass().getSimpleName(), dependencyHealthCheck.isHealthy());
+                    return dependencyHealthCheck.isHealthy();
+                });
     }
 }
